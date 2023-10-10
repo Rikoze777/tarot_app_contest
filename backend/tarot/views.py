fb0b432cc10b550d411aad2fdec5f4b218c9e12f
vardemin <xavierx95@gmail.com>
2023-10-10 19:49:41 +0300

    feat: new bot file, invoice + payments in bot

--- backend/tarot/views.py
+++ backend/tarot/views.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
-from telegram import Bot, LabeledPrice
-from tarot.services.user_utils import get_invoice, get_user_sub
+from telegram import Bot
+from tarot.services.user_utils import create_invoice, get_actual_subscription, get_invoice_params
 from tarot.auth import TelegramAuthentication
 from tarot.services.tarot_utils import check_user_prediction
 from django.conf import settings
+from asgiref.sync import async_to_sync
 
 
 @api_view(['GET'])
@@ -20,11 +21,11 @@ def get_predicton(request, format=None):
 @api_view(['GET'])
 @authentication_classes([TelegramAuthentication])
 def get_user(request, format=None):
-    user_id = request.user.tg_id
-    sub = get_user_sub(user_id)
+    user = request.user
+    sub = get_actual_subscription(user)
     context = {
-        'tg_id': user_id,
-        'level': sub.role
+        'id': user.id,
+        'subscribed': sub is not None
     }
     return Response(context)
 
@@ -32,17 +33,18 @@ def get_user(request, format=None):
 @api_view(['POST'])
 @authentication_classes([TelegramAuthentication])
 def send_invoice(request, format=None):
-    user = request.user
+    invoice = create_invoice(request.user)
+    params = get_invoice_params(invoice)
     bot = Bot(settings.BOT_API_TOKEN)
-    invoice_link = bot.create_invoice_link(
-        "Buy subscription",
-        "Subscription for 1 month",
-        get_invoice(user).uuid.hex,
-        settings.PROVIDER_TOKEN,
-        "RUB",
-        [LabeledPrice("Monthly Subscription", 20000)]
-    )
+    invoice_link = async_to_sync(
+        bot.create_invoice_link
+    )(params['title'],
+      params['description'],
+      params['payload'],
+      settings.PROVIDER_TOKEN,
+      params['currency'],
+      params['prices'])
     resp = {
-        "invoice_link" : invoice_link
+        "invoice_link": invoice_link
     }
     return Response(resp)
